4d8336ce755113d3f9d8694c61cddef810e3bd15
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-03-05 07:48:35 +0700

    create gulp configuration

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack-stream');
+const gulp = require('gulp');
+
+gulp.task('build', () =>
+    gulp.src('index.js')
+        .pipe(webpack({
+          entry: './demo/client.js',
+          output: {
+            library: 'operam',
+            filename: 'operam.js'
+          },
+          externals: {
+            global: 'typeof self !== "undefined" ? self : ' +
+                'typeof window !== "undefined" ? window : ' +
+                'typeof global !== "undefined" ? global : {}'
+          },
+          module: {
+            loaders: [
+              {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                  presets: ['es2015']
+                }
+              }
+            ]
+          }
+        }))
+        .pipe(gulp.dest('demo'))
+);
