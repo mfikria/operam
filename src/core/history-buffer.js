cd4eebccd84ff94a593d901ea8c012647ab9c0fc
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-03-20 21:52:52 +0700

    create history-buffer

---
+++ src/core/history-buffer.js
@@ -0,0 +1,28 @@
+const OperationSequence = require('../helper/operation-sequence');
+
+class HistoryBuffer {
+  constructor(documentId) {
+    this.documentId = documentId;
+    this.operationBuffer = [];
+    this.operationBuffer.push(new OperationSequence([]));
+  }
+
+  latest() {
+    return Promise.resolve(this.operationBuffer.length);
+  }
+
+  until(historyId) {
+    return Promise.resolve(this.operationBuffer.slice(0, historyId));
+  }
+
+  from(historyId) {
+    return Promise.resolve(this.operationBuffer.slice(historyId - 1));
+  }
+
+  store(op) {
+    this.operationBuffer.push(op);
+    return Promise.resolve(this.operationBuffer.length);
+  }
+}
+
+module.exports = HistoryBuffer;
