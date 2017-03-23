86128d60fc370bb09aded05ba019d045ac96a3c5
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-03-23 20:04:27 +0700

    define ot operation

---
+++ src/data-type/ot-operation.js
@@ -0,0 +1,13 @@
+class OTOperation {
+  apply() {
+    throw new Error('Not implemented');
+  }
+
+  invert() {
+    throw new Error('Not implemented');
+  }
+
+  toString() {
+    throw new Error('Not implemented');
+  }
+}
