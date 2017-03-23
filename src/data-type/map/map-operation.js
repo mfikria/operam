23722b222b9da634e18a4306ca02ce9c77dd1105
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-03-23 21:08:03 +0700

    define map operation

---
+++ src/data-type/map/map-operation.js
@@ -0,0 +1,28 @@
+const OTOperation = require('../ot-operation');
+
+class Set extends OTOperation{
+  constructor(key, oldValue, newValue) {
+    super();
+    this.key = key;
+    this.oldValue = oldValue;
+    this.newValue = newValue;
+  }
+
+  apply(handler) {
+    if (this.newValue === null) {
+      handler.remove(this.key, this.oldValue);
+    } else {
+      handler.set(this.key, this.oldValue, this.newValue);
+    }
+  }
+
+  invert() {
+    return new Set(this.key, this.newValue, this.oldValue);
+  }
+
+  toString() {
+    return `Set{key=${this.key}, oldValue=${this.oldValue}, newValue=${this.newValue}}`;
+  }
+}
+
+exports.Set = Set;
