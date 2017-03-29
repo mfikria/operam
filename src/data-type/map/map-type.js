9e31863038a961c454e00268af6a76894bfe1491
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-03-29 17:21:40 +0700

    implement serialize and deserialize object

--- src/data-type/map/map-type.js
+++ src/data-type/map/map-type.js
@@ -19,11 +19,42 @@ class MapType extends OTType {
   }
 
   serializeObject(op) {
+    const result = [];
+    OperationSequence.asArray(op)
+            .forEach((subOp) => {
+              if (subOp instanceof ops.Set) {
+                result.push([
+                  'set',
 
+                  {
+                    key: subOp.key,
+                    oldValue: subOp.oldValue,
+                    newValue: subOp.newValue
+                  }
+                ]);
+              } else {
+                throw new Error(`Unsupported operation: ${subOp}`);
+              }
+            });
+
+    return result;
   }
 
   deserializeObject(json) {
+    const result = [];
 
+    json.forEach((data) => {
+      switch (data[0]) {
+        case 'set':
+          const op = data[1];
+          result.push(new ops.Set(op.key, op.oldValue || null, op.newValue || null));
+          break;
+        default:
+          throw new Error(`Unsupported type of operation: ${data[0]}`);
+      }
+    });
+
+    return new OperationSequence(result);
   }
 }
 
