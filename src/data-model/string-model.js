c4293e6428f171b48d0d05beb0334a8fb8c202c8
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-05-17 20:39:36 +0700

    implement string-model

---
+++ src/data-model/string-model.js
@@ -0,0 +1,143 @@
+const diff = require('fast-diff');
+
+const OTModel = require('./ot-model');
+const string = require('../data-type/string');
+
+class StringModel extends OTModel {
+
+  constructor(workspace) {
+    super(workspace);
+
+    this.value = '';
+    const self = this;
+    workspace.current.apply({
+      retain(count) {
+        throw new Error('Must only contain inserts');
+      },
+
+      delete(value) {
+        throw new Error('Must only contain inserts');
+      },
+
+      insert(value) {
+        self.value += value;
+      }
+    });
+
+    workspace.apply = this.apply.bind(this);
+  }
+
+  apply(data) {
+    const self = this;
+    let index = 0;
+    data.operation.apply({
+      retain(count) {
+        index += count;
+      },
+
+      insert(value) {
+        self.value = self.value.substr(0, index) + value + self.value.substr(index);
+
+        const from = index;
+        index += value.length;
+
+        self.workspace.queueEvent('insert', {
+          index: from,
+          value
+        });
+      },
+
+      delete(value) {
+        self.value = self.value.substr(0, index) + self.value.substr(index + value.length);
+
+        self.workspace.queueEvent('delete', {
+          index,
+          fromIndex: index,
+          toIndex: index + value.length,
+
+          value
+        });
+      }
+    });
+  }
+
+  get() {
+    return this.value;
+  }
+
+  set(value) {
+    if (this.value === value) return;
+
+    const delta = string.delta();
+    const index = 0;
+    diff(this.value, value).forEach((d) => {
+      switch (d[0]) {
+        case diff.EQUAL:
+          delta.retain(d[1].length);
+          break;
+        case diff.INSERT:
+          delta.insert(d[1]);
+
+          break;
+        case diff.DELETE:
+          delta.delete(d[1]);
+          break;
+      }
+    });
+
+    this.workspace.send(delta.done());
+  }
+
+  append(value) {
+    const length = this.value.length;
+    this.workspace.send(string.delta()
+            .retain(length)
+            .insert(value)
+            .done(),
+        );
+  }
+
+  insert(index, value) {
+    const length = this.value.length;
+
+    if (index <= 0) {
+      throw new Error('index invalid');
+    }
+
+    if (index > length) {
+      throw new Error('index invalid');
+    }
+
+    this.workspace.send(string.delta()
+            .retain(index)
+            .insert(value)
+            .retain(length - index)
+            .done(),
+        );
+  }
+
+  remove(fromIndex, toIndex) {
+    if (fromIndex <= 0) {
+      throw new Error('fromIndex invalid');
+    }
+
+    const length = this.value.length();
+    if (toIndex > length) {
+      throw new Error('toIndex invalid');
+    }
+
+    if (toIndex <= fromIndex) {
+      throw new Error('toIndex invalid');
+    }
+
+    const deleted = this.value.substring(fromIndex, toIndex);
+    this.workspace.send(string.delta()
+            .retain(fromIndex)
+            .delete(deleted)
+            .retain(length - toIndex)
+            .done(),
+        );
+  }
+}
+
+module.exports = StringModel;
