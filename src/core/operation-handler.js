1804fc7c6796cfa509860344ba84ac5b7c14fa7d
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-06-03 20:41:36 +0700

    create operation-handler

---
+++ src/core/operation-handler.js
@@ -0,0 +1,32 @@
+const OperationSequence = require('../helper/operation-sequence');
+const ops = require('./operation-wrapper');
+
+class OperationHandler {
+  constructor() {
+    this.ops = [];
+  }
+
+  update(id, type, operation) {
+    if (this.ops[id]) {
+      throw `Can not update id \`${id}\``;
+    }
+
+    this.ops[id] = new ops.OperationWrapper(id, type, operation);
+    return this;
+  }
+
+  done() {
+    const result = [];
+
+    for (const key in this.ops) {
+      if (!this.ops.hasOwnProperty(key)) continue;
+
+      const value = this.ops[key];
+      result.push(value);
+    }
+
+    return new OperationSequence(result);
+  }
+}
+
+module.exports = OperationHandler;
