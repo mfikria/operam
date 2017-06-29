5d51b210d5178f9271f6df130c24ccca2679b22b
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-06-29 13:14:12 +0700

    update client script for demo

--- public/text-editor/text.js
+++ public/text-editor/text.js
@@ -1,31 +1,31 @@
 window.onload = function () {
     // create unique identifier per client
-    var id = uuidv4();
-    var heading = document.getElementById('heading');
-    heading.innerHTML += ': ' + id;
+  const userId = uuidv4();
+  const heading = document.getElementById('heading');
+  heading.innerHTML += `: ${userId}`;
 
-    var socket = io('/');
+  const socket = io('/');
 
     // initialize instance
-    var connector = new operam.connection.SocketConnector(operam.ModelManager.defaultType(), socket, window.location.hash || 'shared');
-    var workspace = new operam.core.Workspace(connector);
-    var modelManager = new operam.ModelManager(workspace);
+  const connector = new operam.connection.SocketConnector(socket, 'text-shared');
+  var currentDocument = new operam.core.Document(connector);
+  const engine = new operam.OTEngine(currentDocument, userId);
 
-    modelManager.open()
-        .then(function () {
-            var title = modelManager.get('title', function () {
-                var result = modelManager.newString();
-                result.set('Testing');
-                return result;
-            });
+  engine.start()
+        .then(() => {
+          const title = engine.get('title', () => {
+            const result = engine.newString();
+            result.set('Testing');
+            return result;
+          });
 
-            var text = modelManager.get('text', function () {
-                var result = modelManager.newString();
-                result.set('Testing content');
-                return result;
-            });
+          const text = engine.get('text', () => {
+            const result = engine.newString();
+            result.set('Testing content');
+            return result;
+          });
 
-            operam.TextareaBinder.bind(text, document.getElementById('text'));
-            operam.TextareaBinder.bind(title, document.getElementById('title'));
+          operam.TextareaBinder.bind(text, document.getElementById('text'));
+          operam.TextareaBinder.bind(title, document.getElementById('title'));
         });
-}
+};
