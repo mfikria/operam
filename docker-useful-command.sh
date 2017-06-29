d83730807551026bb0e27a47b753b4ba7a07b11a
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-06-29 13:14:25 +0700

    add useful command for docker

--- docker-useful-command.sh
+++ docker-useful-command.sh
@@ -1,3 +1,8 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+
+# Edit: Actually I was able to free up space and get going again by removing unused images/containers:
+docker rm $(docker ps -q -f 'status=exited')
+docker rmi $(docker images -q -f "dangling=true")
+docker volume rm $(docker volume ls -qf dangling=true)
