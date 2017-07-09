64c79b353279a84e8b548d8075f582b405b8bb57
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-07-09 11:35:19 +0700

    fix socket.io on client

--- public/text-editor/text.js
+++ public/text-editor/text.js
@@ -1,14 +1,14 @@
 window.onload = function () {
     // create unique identifier per client
-  const userId = Date.now() + "_" + Math.floor(Math.random() * 2000);
+  const userId = `${Date.now()}_${Math.floor(Math.random() * 2000)}`;
   const heading = document.getElementById('heading');
   heading.innerHTML += `: ${userId}`;
 
-  const socket = io('/');
+  const socket = io('/', { transports: ['websocket'] });
 
     // initialize instance
   const connector = new operam.connection.SocketConnector(socket, 'text-shared');
-  var currentDocument = new operam.core.Document(connector);
+  const currentDocument = new operam.core.Document(connector);
   const engine = new operam.OTEngine(currentDocument, userId);
 
   engine.start()
