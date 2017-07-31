2643398d861bc20081e00fb3cceca625209c55f8
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-07-31 07:14:59 +0700

    implement reconnection method

--- src/core/document-manager.js
+++ src/core/document-manager.js
@@ -79,24 +79,19 @@ class DocumentManager {
     //
     //         return Promise.resolve(arr);
     //       });
-    return this.lock((done) => {
-      let composedOperation;
-      this.historyBuffer.from(historyId + 1)
+    // return this.lock((done) => {
+    let composedOperation;
+    this.historyBuffer.from(historyId + 1)
               .then((ops) => {
                 const composer = this.operationManager.newOperationComposer();
                 ops.forEach((op) => {
                   composer.add(op);
                 });
                 composedOperation = composer.done();
-
-                return this.historyBuffer.latest();
-              })
-              .then((id) => {
                 const operationId2 = uuidv4();
-                done(null, new OperationBundle(id, operationId2, composedOperation));
-              })
-              .catch(err => done(err));
-    });
+                return Promise.resolve(new OperationBundle(this.historyBuffer.latest(), operationId2, composedOperation));
+              });
+    // });
   }
 }
 
